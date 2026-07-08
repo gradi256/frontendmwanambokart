039a2ec3a92a9a84c18c8e42bdc30e1c91b09e2d
Gradi Kusanika <gradinawej8@gmail.com>
2026-07-08 22:05:46 +0100

    feat : installation de sonner pour les notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { RouterProvider } from "react-router-dom"
 import { router } from "./routes"
-import {  QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { Toaster } from "sonner"
 
 const queryClient = new QueryClient()
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
+      <Toaster position="top-center" richColors />
       <RouterProvider router={router} />
     </QueryClientProvider>
   )
