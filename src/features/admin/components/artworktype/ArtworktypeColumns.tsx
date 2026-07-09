b6078b6573ccafbd3b6473e33e8668c4a959cbc1
Gradi Kusanika <gradinawej8@gmail.com>
2026-07-09 09:30:08 +0100

    feat : Modification des colonnes

--- src/features/admin/components/artworktype/ArtworktypeColumns.tsx
+++ src/features/admin/components/artworktype/ArtworktypeColumns.tsx
@@ -1,17 +1,64 @@
 import { type ColumnDef } from "@tanstack/react-table"
 import type { ArtworktypeType } from "../../types/ArtworktypeType"
 
+import { MoreHorizontal } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 export const artworktypeColumns: ColumnDef<ArtworktypeType>[] = [
   {
-    accessorKey: "id_type", 
-    header: "Id du type",
+    accessorKey: "id_type",
+    header: () => <div className="">Identifiant</div>,
   },
   {
-    accessorKey: "name", 
+    accessorKey: "name",
     header: "Nom de la catégorie",
   },
   {
-    accessorKey: "description", 
+    accessorKey: "description",
     header: "Description",
+    cell: ({ row }) => {
+      const description = row.getValue("description") as string
+      return description.length > 50
+        ? `${description.substring(0, 50)}...`
+        : description
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.id_type)}
+            >
+              Copier l'Id
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Voir plus</DropdownMenuItem>
+            <DropdownMenuItem>Modifier</DropdownMenuItem>
+            <DropdownMenuItem>Suppimer</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
   },
 ]
