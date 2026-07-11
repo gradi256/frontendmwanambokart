46700b114abe67c16817d92bb8f44a312b8a1adc
Gradi Kusanika <gradinawej8@gmail.com>
2026-07-11 18:11:15 +0100

    fix : fixe du delete artist

--- src/features/admin/types/ArtistType.tsx
+++ src/features/admin/types/ArtistType.tsx
@@ -1,19 +1,18 @@
 export type ArtistType = {
-  id_artist: string;
-  userId: string;
-  name: string;
-  prenom: string;
-  pseudo: string;
-  phone: string;
-  pays: string;
-  sexe: string;
-  biographie: string;
-  profile: string;
-  createdAt: string;
-  updatedAt: string;
-  user: {
-    id_user: string;
-    email: string;
-    role: string;
+  id_user: string; // Gardé si utilisé à la racine quelque part
+  email: string;
+  artist: {
+    id_artist: string;
+    userId: string;
+    name: string;
+    prenom: string;
+    pseudo: string;
+    phone: string;
+    pays: string;
+    sexe: string;
+    biographie: string;
+    profile: string;
+    createdAt: string;
+    updatedAt: string;
   };
 }
